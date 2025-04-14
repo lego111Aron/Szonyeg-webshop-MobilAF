40551c2bcd82d0c031018bb363e6d0e788e906b5
Palfi Aron <h376388@stud.u-szeged.hu>
2025-04-14 20:05:12 +0200

    sokminden

--- app/src/main/java/com/example/szonyeg_webshop_mobilaf/RegisterActivity.java
+++ app/src/main/java/com/example/szonyeg_webshop_mobilaf/RegisterActivity.java
@@ -30,61 +30,46 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class RegisterActivity extends AppCompatActivity {
     private static final String LOG_TAG = RegisterActivity.class.getName();
     private static final String PREF_KEY = RegisterActivity.class.getPackage().toString();
     private static final int SECRET_KEY = 99;
+
     EditText userNameEditText;
     EditText userEmailEditText;
     EditText passwordEditText;
     EditText passwordConfirmEditText;
-    EditText phoneEditText;
-    Spinner spinner;
+    EditText postalCodeEditText;
     EditText addressEditText;
-    RadioGroup accountTypeGroup;
-
+    RadioGroup termsRadioGroup;
 
     private SharedPreferences preferences;
     private FirebaseAuth mAuth;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-//        Bundle bundle = getIntent().getExtras();
-//        int secret_key = bundle.getInt("SECRET_KEY");
+
         int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
-        if(secret_key != 99){
+        if (secret_key != SECRET_KEY) {
             finish();
         }
+
         userNameEditText = findViewById(R.id.userNameEditText);
         userEmailEditText = findViewById(R.id.userEmailText);
         passwordEditText = findViewById(R.id.passwordEditText);
         passwordConfirmEditText = findViewById(R.id.passwordAgainEditText);
-        phoneEditText = findViewById(R.id.phoneEditText);
-        spinner=findViewById(R.id.phoneSpinner);
+        postalCodeEditText = findViewById(R.id.postalCodeEditText);
         addressEditText = findViewById(R.id.addressEditText);
-        accountTypeGroup = findViewById(R.id.accountTypeGroup);
-        accountTypeGroup.check(R.id.buyerRadioButton);
+        termsRadioGroup = findViewById(R.id.termsRadioGroup);
 
         preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
         String userName = preferences.getString("userName", "");
-        String password = preferences.getString("password","");
+        String password = preferences.getString("password", "");
 
         userNameEditText.setText(userName);
         passwordEditText.setText(password);
-
-        spinner.setOnItemSelectedListener(this);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.phone_modes, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -97,22 +82,19 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         String password = passwordEditText.getText().toString();
         String passwordConfirm = passwordConfirmEditText.getText().toString();
 
-        if(!password.equals(passwordConfirm)){
-            Log.e(LOG_TAG, "Nem ugyan az a jelszó és a megerősítés.");
+        if (!password.equals(passwordConfirm)) {
+            Log.e(LOG_TAG, "Passwords do not match.");
+            Toast.makeText(this, "Passwords do not match.", Toast.LENGTH_SHORT).show();
             return;
         }
-        String phoneNumber = phoneEditText.getText().toString();
-        String phoneType = spinner.getSelectedItem().toString();
-        String address = addressEditText.getText().toString();
 
+        int checkedId = termsRadioGroup.getCheckedRadioButtonId();
+        if (checkedId == R.id.declineTermsRadioButton) {
+            Toast.makeText(this, "You must accept the terms and conditions to register.", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        int checkedId = accountTypeGroup.getCheckedRadioButtonId();
-        RadioButton radioButton = accountTypeGroup.findViewById(checkedId);
-        String accountType = radioButton.getText().toString();
-
-
-        Log.i(LOG_TAG, "Regisztrált: " + userName + ", e-mail: " + email);
-//        startShopping();
+        Log.i(LOG_TAG, "Registering user: " + userName + ", email: " + email);
 
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
@@ -121,22 +103,22 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
                     Log.d(LOG_TAG, "User created successfully");
                     startShopping();
                 } else {
-                    Log.d(LOG_TAG, "User wasn`t created successfully");
-                    Toast.makeText(RegisterActivity.this, "User wasn`t created successfully" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    Log.d(LOG_TAG, "User creation failed");
+                    Toast.makeText(RegisterActivity.this, "Registration failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    public void cancle(View view) {
+    public void cancel(View view) {
         finish();
     }
 
-    private void startShopping(/* register user data */) {
+    private void startShopping() {
         Intent intent = new Intent(this, ShopListActivity.class);
-//        intent.putExtra("SECRET_KEY", SECRET_KEY);
         startActivity(intent);
     }
+
 
     @Override
     protected void onRestart() {
@@ -174,15 +156,15 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         Log.i(LOG_TAG, "onResume");
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String selectedItem = parent.getItemAtPosition(position).toString();
-        Log.i(LOG_TAG, selectedItem);
-        //Todo
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        //todo
-    }
+//    @Override
+//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//        String selectedItem = parent.getItemAtPosition(position).toString();
+//        Log.i(LOG_TAG, selectedItem);
+//        //Todo
+//    }
+//
+//    @Override
+//    public void onNothingSelected(AdapterView<?> parent) {
+//        //todo
+//    }
 }
--- app/src/main/java/com/example/szonyeg_webshop_mobilaf/ShopListActivity.java
+++ app/src/main/java/com/example/szonyeg_webshop_mobilaf/ShopListActivity.java
@@ -1,41 +1,58 @@
 package com.example.szonyeg_webshop_mobilaf;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ShopListActivity extends AppCompatActivity {
-
     private static final String LOG_TAG = ShopListActivity.class.getName();
     private FirebaseUser user;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_shop_list);
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        */
 
-        user = FirebaseAuth.getInstance().getCurrentUser();
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
         if (user != null) {
             Log.d(LOG_TAG, "Authenticated user!");
         } else {
-            Log.d(LOG_TAG, "Unauthenticated user!");
+            Log.d(LOG_TAG, "Unauthenticated user! Redirecting...");
             finish();
         }
+
+        TextView titleText = findViewById(R.id.titleText);
+        TextView subtitleText = findViewById(R.id.subtitleText);
+
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        titleText.startAnimation(fadeIn);
+        subtitleText.startAnimation(fadeIn);
+
+        Button logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(view -> {
+            mAuth.signOut();
+            Log.d(LOG_TAG, "User signed out");
+
+            Intent intent = new Intent(ShopListActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        });
     }
 }
